445906c247066ac765b8dd56b0ab2805ee5fb021
Алексей Прохин <alexsey.prohin@glonassmobile.com>
2022-01-16 17:01:32 +0300

    add gitignore, buildgradle, open test

---
+++ src/test/java/github/SearchSelenide.java
@@ -0,0 +1,18 @@
+package github;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchSelenide {
+
+
+    @Test
+    void shouldFindeSelenoidRepositoryGithub(){
+        open("https://github.com/");
+        // open github.com
+        // enter text selenide and click Enter
+        // click in firts link
+        // check
+    }
+}
